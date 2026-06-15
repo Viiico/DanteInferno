9b6c24ff3377b92d236110c7f862753e9ee21da2
Viiico <wiktorkwv@gmail.com>
2026-06-15 22:07:11 +0200

    Calculating setup profits

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 import { prepareItemContent, prepareNeededItems } from "./helperFuncs/itemPreparation";
+import { calculateSetupDrops, readMinionSetup } from "./helperFuncs/minionSetupHandler";
 import { resolveItemPrices } from "./resolveItemPrices";
 
-const itemPrices = await resolveItemPrices();
+const pricedItems = await resolveItemPrices();
+const minionSetup = await readMinionSetup();
+const setupDrops = calculateSetupDrops(minionSetup);
+const setupProfit = Object.entries(setupDrops).reduce((acc, [productName, productAmount]) => {
+    const itemPrice = pricedItems.get(productName)!?.cheapest.cost;
+    return acc + itemPrice * productAmount;
+}, 0).toFixed(0);
 
-console.log(itemPrices)
+console.log(setupProfit)
